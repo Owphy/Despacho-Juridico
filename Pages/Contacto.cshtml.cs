149e8957bad552370ed68461877c7d5c437340e2
Ivan ZL <132529789+PredicateDog@users.noreply.github.com>
2026-07-20 20:30:22 -0600

    DespachoJuridico V1

---
+++ Pages/Contacto.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Despacho_Juridico.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
